4dcf92fc930b0fda5a461946f6c66bf682ace2ff
BasileLT <blt_git@pm.me>
2025-12-09 14:30:48 +0000

    remove pycache

--- 2.Elements-de-theorie-de-l-information/td2.py
+++ 2.Elements-de-theorie-de-l-information/td2.py
@@ -6,5 +6,10 @@ Created on Tue Dec  9 15:22:15 2025
 
 import numpy as np
 from descente_stochastique import GradientDescent
+import pandas as pd
 
 
+df = pd.read_csv('titanic2.csv')
+
+def grad_cout(data):
+    
