59b99ecd1606a510fba416a546aac23e434ce217
BasileLT <blt_git@pm.me>
2026-01-08 12:36:53 +0000

    exam

--- old_exam/examen_MDS_NOM_Prenom.py
+++ old_exam/examen_MDS_NOM_Prenom.py
@@ -18,29 +18,31 @@ from sklearn.preprocessing import StandardScaler
 
 ### EXERCICE 1
 
+
 def heaviside(x):
     return tf.where(x >= 0, 1.0, 0.0)
 
+
 # Fonction d'activation personnalisée
-get_custom_objects().update({'heaviside': Activation(heaviside)})
+get_custom_objects().update({"heaviside": Activation(heaviside)})
 
 # Architecture du réseau
 modele = Sequential()
 
 # Couches de neurones
-modele.add(Dense(None, input_dim=None, activation = 'heaviside')) #à compléter
-modele.add(Dense(None, activation = 'heaviside')) #à compléter
+modele.add(Dense(None, input_dim=None, activation="heaviside"))  # à compléter
+modele.add(Dense(None, activation="heaviside"))  # à compléter
 
 # Couche 1
-coeff = np.array(None) #à compléter
-biais = np.array(None) #à compléter
-poids = [coeff,biais]
+coeff = np.array(None)  # à compléter
+biais = np.array(None)  # à compléter
+poids = [coeff, biais]
 modele.layers[0].set_weights(poids)
 
 # Couche 2
-coeff = np.array(None) #à compléter
-biais = np.array(None) #à compléter
-poids = [coeff,biais]
+coeff = np.array(None)  # à compléter
+biais = np.array(None)  # à compléter
+poids = [coeff, biais]
 modele.layers[1].set_weights(poids)
 
 # Affichage
@@ -54,27 +56,45 @@ z_grid = modele.predict(xy_grid).reshape(x_grid.shape)
 
 # Visualisation des résultats en 3D
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(x_grid, y_grid, z_grid, cmap='viridis')
+ax = fig.add_subplot(111, projection="3d")
+ax.plot_surface(x_grid, y_grid, z_grid, cmap="viridis")
 
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('f(x, y)')
-ax.set_title('Sortie du réseau sur [-5, 5]^2')
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("f(x, y)")
+ax.set_title("Sortie du réseau sur [-5, 5]^2")
 plt.show()
 
 
+### EXERCICE 3
 
-### EXERCICE 3 
 
 def read_data():
-    housing_data = np.genfromtxt('housing.csv', delimiter=',')
+    housing_data = np.genfromtxt("housing.csv", delimiter=",")
     return housing_data
+
 
 housing_data = read_data()
 
+## Q1
 X = housing_data[:, :-1]  # Toutes les colonnes sauf la dernière
-y = housing_data[:, -1]   # Seulement la dernière colonne
+y = housing_data[:, -1]  # Seulement la dernière colonne
 scale = StandardScaler()
 X_scaled = scale.fit_transform(X)
-print("\n1.Revenue médian standardisé (1er groupe) :", np.median(y))
+valeur_q1 = X_scaled[0, 0]
+print("\nQ1. Revenue médian standardisé (1er groupe) :", valeur_q1)
+
+
+## Q2
+reg = LinearRegression()
+reg.fit(X_scaled, y)
+
+coefs_str = ", ".join(["{:.2f}".format(c) for c in reg.coef_])
+print(f"\nQ2. Solution OLS :")
+print(f"    Intercept : {reg.intercept_:.2f}")
+print(f"    Coefficients : [{coefs_str}]")
+
+## Q3
+x_g1 = X_scaled[0].reshape(1, -1)
+pred_g1 = reg.predict(x_g1)[0]
+vrai_prix = y[0]
